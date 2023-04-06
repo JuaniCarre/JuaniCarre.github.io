7d665eee2bc4f90195a6b34a858dd42420d20508
Juani <juanicarrenio@gmail.com>
2023-04-06 19:21:26 -0300

    initial commit

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+  return(
+    <div>
+      <p className="uppercase">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis accusantium aperiam sapiente culpa saepe? Blanditiis vel corporis reiciendis, fuga sapiente cum amet similique, dolore perferendis eligendi fugit consequuntur laudantium ad?</p>
+    </div>
+  )
+}
