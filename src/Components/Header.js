da5fbce6f365a78741748e7edfeeb6e380314807
Juani <juanicarrenio@gmail.com>
2023-09-07 19:50:45 -0300

    responsive almost done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { useTranslation } from 'react-i18next'
 import spanish from '../images/spanish.png'
 import english from '../images/english.png'
 
 function Header(){
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [t, i18n] = useTranslation("global")
 
   function toAbout(){
@@ -63,25 +63,32 @@ function Header(){
 
 
   return(
-    <header id="navbar" className='navbar'>
-    <div className="navContainer" id='navContainer'>
-    <ul className='items'>
-      <a rel="noreferrer" href='https://drive.google.com/file/d/1l-WucNr9fjhvd9mkR95QxHHzPzbV4qm0/view' target="_blank"><li className='listItem'>{t("header.CV")}</li></a>
-      <li className='listItem' onClick={e=>toAbout()}>{t("header.AboutMe")}</li>
-      <li className='listItem' onClick={e=>toSkills()}>{t("header.Skills")}</li>
-      <li className='listItem' onClick={e=>toProjects()}>{t("header.Projects")}</li>
-      <li className='listItem' onClick={e=>toContact()}>{t("header.Contact")}</li>
-    </ul>
-    <div className='language'>
-      <button onClick={()=>{i18n.changeLanguage("es")}} className='languageButton'>
-        <img className='languageButtonImg' alt='Spanish' src={spanish}/>
-      </button>
-      <p className='separator'>-</p>
-      <button onClick={()=>{i18n.changeLanguage("en")}} className='languageButton'>
-        <img className='languageButtonImg' alt='English' src={english}/>
-      </button>
-    </div>
-    </div>
+    <header id="navbar" className="navbar">
+      <div className="navContainer" id="navContainer">
+        <div className="burger-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+          <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+          <div className={`bar ${isMenuOpen ? "open" : ""}`}></div>
+        </div>
+        
+        <ul className={`items ${isMenuOpen ? "open" : ""}`}>
+          <a rel="noreferrer" href='https://drive.google.com/file/d/1l-WucNr9fjhvd9mkR95QxHHzPzbV4qm0/view' target="_blank"><li className='listItem'>{t("header.CV")}</li></a>
+          <li className='listItem' onClick={e=>toAbout()}>{t("header.AboutMe")}</li>
+          <li className='listItem' onClick={e=>toSkills()}>{t("header.Skills")}</li>
+          <li className='listItem' onClick={e=>toProjects()}>{t("header.Projects")}</li>
+          <li className='listItem' onClick={e=>toContact()}>{t("header.Contact")}</li>
+        </ul>
+
+        <div className='language'>
+          <button onClick={()=>{i18n.changeLanguage("es")}} className='languageButton'>
+            <img className='languageButtonImg' alt='Spanish' src={spanish}/>
+          </button>
+          <p className='separator'>-</p>
+          <button onClick={()=>{i18n.changeLanguage("en")}} className='languageButton'>
+            <img className='languageButtonImg' alt='English' src={english}/>
+          </button>
+        </div>
+      </div>
     </header>
   )
 }
--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -10,12 +10,16 @@ function Projects(){
   return(
     <div className="ProjectsContainer" id="projects">
       <h2 className="ProjectTag">{t("Projects.Projects")}</h2>
+
       <div className="ProjectContainer">
-        <img src={autoBid} alt="Imagen o video del proyecto" className="ProjectImage"></img>
-        <div className="ProjectText">
-          <h3 className="ProjectTitle">{t("Projects.Project1.title")}</h3>
-          <h5 className="ProjectRole">{t("Projects.Project1.role")}</h5>
-          <p className="ProjectDescription">{t("Projects.Project1.description")}</p>
+          <div className='ProjectDivider'>
+            <img src={autoBid} alt="Imagen o video del proyecto" className="ProjectImage"></img>
+            <div className="ProjectText">
+              <h3 className="ProjectTitle">{t("Projects.Project1.title")}</h3>
+              <h5 className="ProjectRole">{t("Projects.Project1.role")}</h5>
+              <p className="ProjectDescription">{t("Projects.Project1.description")}</p>
+            </div>
+          </div>
           <ul className='ProjectLinks'>
             <button className='ProjectButton'>
               <a rel="noreferrer" className='ProjectLink' target='_blank' href='https://github.com/matumoralli/AutoBid-API'><li>{t("Projects.Project1.repo")}</li></a>
@@ -24,41 +28,46 @@ function Projects(){
               <a rel="noreferrer" className='ProjectLink' href='https://autobidmarket.com/' target='_blank'><li>{t("Projects.Project1.deploy")}</li></a>
             </button>
           </ul>
-        </div>
       </div>
+
       <div className="ProjectContainer">
-        <img src={luxury} alt="Imagen o video del proyecto" className="ProjectImage"></img>
-        <div className="ProjectText">
-          <h3 className="ProjectTitle">{t("Projects.Project2.title")}</h3>
-          <h5 className="ProjectRole">{t("Projects.Project2.role")}</h5>
-          <p className="ProjectDescription">{t("Projects.Project2.description")}</p>
-          <ul className='ProjectLinks'>
-            <button className='ProjectButton'>
-              <a rel="noreferrer" className='ProjectLink' target='_blank' href='https://github.com/Santiago-Miranda/HENRY-LUXURY'><li>{t("Projects.Project1.repo")}</li></a>
-            </button>
-            <button className='ProjectButton'>
-              <li onClick={()=> {alert("This project is not deployed. you can check the repository!")}}>{t("Projects.Project1.deploy")}</li>
-            </button>
-          </ul>
+        <div className='ProjectDivider'>
+          <img src={luxury} alt="Imagen o video del proyecto" className="ProjectImage"></img>
+          <div className="ProjectText">
+            <h3 className="ProjectTitle">{t("Projects.Project2.title")}</h3>
+            <h5 className="ProjectRole">{t("Projects.Project2.role")}</h5>
+            <p className="ProjectDescription">{t("Projects.Project2.description")}</p>
+          </div>
         </div>
+        <ul className='ProjectLinks'>
+          <button className='ProjectButton'>
+            <a rel="noreferrer" className='ProjectLink' target='_blank' href='https://github.com/Santiago-Miranda/HENRY-LUXURY'><li>{t("Projects.Project1.repo")}</li></a>
+          </button>
+          <button className='ProjectButton'>
+            <li className='ProjectLink' onClick={()=> {alert("This project is not deployed. you can check the repository!")}}>{t("Projects.Project1.deploy")}</li>
+          </button>
+        </ul>
       </div>
+
       <div className="ProjectContainer">
-        <img src={pokemon} alt="Imagen o video del proyecto" className="ProjectImage"></img>
-        <div className="ProjectText">
-          <h3 className="ProjectTitle">{t("Projects.Project3.title")}</h3>
-          <h5 className="ProjectRole">{t("Projects.Project3.role")}</h5>
+        <div className='ProjectDivider'>
+          <img src={pokemon} alt="Imagen o video del proyecto" className="ProjectImage"></img>
+          <div className="ProjectText">
+            <h3 className="ProjectTitle">{t("Projects.Project3.title")}</h3>
+            <h5 className="ProjectRole">{t("Projects.Project3.role")}</h5>
           <p className="ProjectDescription">{t("Projects.Project3.description")}</p>
+          </div>
+        </div>
           <ul className='ProjectLinks'>
             <button className='ProjectButton'>
               <a rel="noreferrer" className='ProjectLink' target='_blank' href='https://github.com/JuaniCarre/PI-pokemon'><li>{t("Projects.Project1.repo")}</li></a>
             </button>
             <button className='ProjectButton'>
-              <li onClick={()=> {alert("This project is not deployed. you can check the repository!")}}>{t("Projects.Project1.deploy")}</li>
+              <li className='ProjectLink' onClick={()=> {alert("This project is not deployed. you can check the repository!")}}>{t("Projects.Project1.deploy")}</li>
             </button>
           </ul>
-        </div>
       </div>
-    </div>
+    </div> 
   )
 }
 
